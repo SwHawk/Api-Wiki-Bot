5fde30324809ee0d3d5591ba3932cd2073e7984c
SwHawk <darkdeathangel38@gmail.com>
2014-07-09 19:17:36 +0200

    Création d'une classe pour les constantes de correspondace

---
+++ src/SWHawkBot/Constants.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace SWHawkBot;
+
+abstract class Constants
+{
+    /**
+     * Constantes de correspondances API/Wiki-fr
+     * pour les différentes pièces d'armure
+     */
+
+    const API_ARMOR_WEIGHT_HEAVY = "Heavy";
+
+    const ARMOR_WEIGHT_HEAVY = "Lourd";
+
+    const API_ARMOR_WEIGHT_MEDIUM = "Medium";
+
+    const ARMOR_WEIGHT_MEDIUM = "Intermédiaire";
+
+    const API_ARMOR_WEIGHT_LIGHT = "Light";
+
+    const ARMOR_WEIGHT_LIGHT = "Léger";
+
+    const API_ARMOR_TYPE_BOOTS = "Boots";
+
+    const ARMOR_TYPE_BOOTS = "Botte";
+
+    const API_ARMOR_TYPE_COAT = "Coat";
+
+    const ARMOR_TYPE_COAT = "Manteau";
+
+    const API_ARMOR_TYPE_GLOVES = "Gloves";
+
+    const ARMOR_TYPE_GLOVES = "Gant";
+
+    const API_ARMOR_TYPE_HELM = "Helm";
+
+    const ARMOR_TYPE_HELM = "Heaume";
+
+    const API_ARMOR_TYPE_HELMAQUATIC = "HelmAquatic";
+
+    const ARMOR_TYPE_HELMAQUATIC = "Heaume";
+
+    const API_ARMOR_TYPE_LEGGINGS = "Leggings";
+
+    const ARMOR_TYPE_LEGGINGS = "Jambière";
+
+    const API_ARMOR_TYPE_SHOULDERS = "Shoulders";
+
+    const ARMOR_TYPE_SHOULDERS = "Epaulière";
+
+    /**
+     * Constantes de correspondance API/Wiki-fr
+     * pour les sacs
+     */
+
+    const API_TYPE_BAG = "Bag";
+
+    const TYPE_BAG = "Sac";
+
+    /**
+     * Constantes de correspondace API/Wiki-fr
+     * pour les conteneurs
+     */
+
+    const API_TYPE_BULK = "Bulk";
+
+    const TYPE_BULK = "En vrac";
+
+    /**
+     * Constantes de correspondance API/Wiki-fr
+     * pour les métiers d'artisanat
+     */
+
+    const API_DISCIPLINE_ARMORSMITH = "Armorsmith";
+
+    const DISCIPLINE_ARMORSMITH = "Forgeron d'armures";
+
+    const API_DISCIPLINE_ARTIFICER = "Artificer";
+
+    const DISCIPLINE_ARTIFICER = "Artificier";
+
+    const API_DISCIPLINE_CHEF = "Chef";
+
+    const DISCIPLINE_CHEF = "Maître-queux";
+
+    const API_DISCIPLINE_HUNTSMAN = "Huntsman";
+
+    const DISCIPLINE_HUNTSMAN = "Chasseur";
+
+    const API_DISCIPLINE_JEWELER = "Jeweler";
+
+    const DISCIPLINE_JEWELER = "Bijoutier";
+
+    const API_DISCIPLINE_LEATHERWORKER = "Leatherworker";
+
+    const DISCIPLINE_LEATHERWORKER = "Travailleur du cuir";
+
+    const API_DISCIPLINE_TAILOR = "Tailor";
+
+    const DISCIPLINE_TAILOR = "Tailleur";
+
+    const API_DISCIPLINE_WEAPONSMITH = "Weaponsmith";
+
+    const DISCIPLINE_WEAPONSMITH = "Forgeron d'armes";
+
+    /**
+     * Constantes de correspondance API/Wiki-fr
+     * pour les accessoires
+     */
+
+    const API_TRINKET_TYPE_RING = "Ring";
+
+    const TRINKET_TYPE_RING = "Bague";
+
+    const API_TRINKET_TYPE_AMULET = "Amulet";
+
+    const TRINKET_TYPE_AMULET = "Amulette";
+
+    const API_TRINKET_TYPE_EARRING = "Earring";
+
+    const TRINKET_TYPE_EARRING = "Boucle d'oreille";
+
+    const API_TRINKET_TYPE_TRINKET = "Trinket";
+
+    const TRINKET_TYPE_TRINKET = "Accessoire";
+
+    /**
+     * Constantes de correspondance API/Wiki-fr
+     * pour les armes
+     */
+
+    const API_WEAPON_TYPE_AXE = "Axe";
+
+    const WEAPON_TYPE_AXE = "Hache";
+
+    const API_WEAPON_TYPE_DAGGER = "Dagger";
+
+    const WEAPON_TYPE_DAGGER = "Dague";
+
+    const API_WEAPON_TYPE_FOCUS = "Focus";
+
+    const WEAPON_TYPE_FOCUS = "Focus";
+
+    const API_WEAPON_TYPE_GREATSWORD = "Greatsword";
+
+    const WEAPON_TYPE_GREATSWORD = "Espadon";
+
+    const API_WEAPON_TYPE_HAMMER = "Hammer";
+
+    const WEAPON_TYPE_HAMMER = "Marteau";
+
+    const API_WEAPON_TYPE_HARPOON = "Harpoon";
+
+    const WEAPON_TYPE_HARPOON = "Lance";
+
+    const API_WEAPON_TYPE_LONGBOW = "LongBow";
+
+    const WEAPON_TYPE_LONGBOW = "Arc long";
+
+    const API_WEAPON_TYPE_MACE = "Mace";
+
+    const WEAPON_TYPE_MACE = "Masse";
+
+    const API_WEAPON_TYPE_PISTOL = "Pistol";
+
+    const WEAPON_TYPE_PISTOL = "Pistolet";
+
+    const API_WEAPON_TYPE_RIFLE = "Rifle";
+
+    const WEAPON_TYPE_RIFLE = "Fusil";
+
+    const API_WEAPON_TYPE_SCEPTER = "Scepter";
+
+    const WEAPON_TYPE_SCEPTER = "Sceptre";
+
+    const API_WEAPON_TYPE_SHIELD = "Shield";
+
+    const WEAPON_TYPE_SHIELD = "Bouclier";
+
+    const API_WEAPON_TYPE_SHORTBOW = "ShortBow";
+
+    const WEAPON_TYPE_SHORTBOW = "Arc court";
+
+    const API_WEAPON_TYPE_SPEARGUN = "Speargun";
+
+    const WEAPON_TYPE_SPEARGUN = "Fusil-harpon";
+
+    const API_WEAPON_TYPE_STAFF = "Staff";
+
+    const WEAPON_TYPE_STAFF = "Bâton";
+
+    const API_WEAPON_TYPE_SWORD = "Sword";
+
+    const WEAPON_TYPE_SWORD = "Epée";
+
+    const API_WEAPON_TYPE_TORCH = "Torch";
+
+    const WEAPON_TYPE_TORCH = "Torche";
+
+    const API_WEAPON_TYPE_TRIDENT = "Trident";
+
+    const WEAPON_TYPE_TRIDENT = "Trident";
+
+    const API_WEAPON_TYPE_WARHORN = "Warhorn";
+
+    const WEAPON_TYPE_WARHORN = "Cor de guerre";
+
+    /**
+     * Constantes de correspondance API/Wiki-fr
+     * pour les objets de maître-queux
+     */
+
+    const API_TYPE_CONSUMABLE = "Consumable";
+
+    const TYPE_CONSUMABLE = "Consommable";
+
+    const API_CHEF_TYPE_DESSERT = "Dessert";
+
+    const CHEF_TYPE_DESSERT = "Dessert";
+
+    const API_CHEF_TYPE_DYE = "Dye";
+
+    const CHEF_TYPE_DYE = "Teinture";
+
+    const API_CHEF_TYPE_FEAST = "Feast";
+
+    const CHEF_TYPE_FEAST = "Festin";
+
+    const API_CHEF_TYPE_INGREDIENT_COOKING = "IngredientCooking";
+
+    const CHEF_TYPE_INGREDIENT_COOKING = "Ingrédient Culinaire";
+
+    const API_CHEF_TYPE_MEAL = "Meal";
+
+    const CHEF_TYPE_MEAL = "Plat";
+
+    const API_CHEF_TYPE_SEASONING = "Seasoning";
+
+    const CHEF_TYPE_SEASONING = "Assaisonnement";
+
+    const API_CHEF_TYPE_SNACK = "Snack";
+
+    const CHEF_TYPE_SNACK = "En-cas";
+
+    const API_CHEF_TYPE_SOUP = "Soup";
+
+    const CHEF_TYPE_SOUP = "Soupe";
+
+    /**
+     * Constantes de correspondance API/Wiki-fr
+     * pour les matériaux d'artisanat
+     */
+    const API_TYPE_COMPONENT = "Component";
+
+    const TYPE_COMPONENT = "Composant d'artisanat";
+
+    const API_TYPE_POTION = "Potion";
+
+    const TYPE_POTION = "Potion";
+
+    const API_TYPE_REFINEMENT = "Refinement";
+
+    const TYPE_REFINEMENT = "Rafinnage";
+
+    const API_TYPE_REFINEMENT_ECTOPLASM = "RefinementEctoplasm";
+
+    const TYPE_REFINEMENT_ECTOPLASM = "Raffinage d'ectoplasme";
+
+    const API_TYPE_REFINEMENT_OBSIDIAN = "RefinementObsidian";
+
+    const TYPE_REFINEMENT_OBSIDIAN = "Raffinage d'obsidienne";
+
+    const API_TYPE_UPGRADE_COMPONENT = "UpgradeComponent";
+
+    const TYPE_UPGRADE_COMPONENT = "Composant d'amélioration";
+
+}
