5055a4ef62bca13606211f1bfda51d822d882555
SwHawk <darkdeathangel38@gmail.com>
2014-07-23 17:09:06 +0200

    Mise à jour de la factory pour les nouvelles classes créées

--- src/SWHawkBot/Factories/ItemFactory.php
+++ src/SWHawkBot/Factories/ItemFactory.php
@@ -3,43 +3,75 @@ namespace SWHawkBot\Factories;
 
 use SWHawkBot\GW2ApiBot\GW2ItemBot;
 use SWHawkBot\Constants;
-use SWHawkBot\Entities\Weapon;
 use SWHawkBot\Entities\Armor;
 use SWHawkBot\Entities\Bag;
-use SWHawkBot\Entities\Trinket;
+use SWHawkBot\Entities\Back;
+use SWHawkBot\Entities\Consumable;
 use SWHawkBot\Entities\Container;
+use SWHawkBot\Entities\CraftingMaterial;
+use SWHawkBot\Entities\Trinket;
+use SWHawkBot\Entities\Trophy;
+use SWHawkBot\Entities\UpgradeComponent;
+use SWHawkBot\Entities\Weapon;
 
 class ItemFactory
 {
 
     public static function returnItem($id)
     {
-        $itemBot = GW2ItemBot::getItemBotInstance("1", "fr");
+        $itemBot = GW2ItemBot::getItemBotInstance("1", "fr", 3);
         $itemRaw = $itemBot->getItemRaw($id);
+        $itemTypesArray = Constants::$translation['item_types'];
         if (!is_null($itemRaw)) {
             if (isset($itemRaw[strtolower($itemRaw['type'])]['type'])) {
                 $itemSpecificType = $itemRaw[strtolower($itemRaw['type'])]['type'];
-                if (array_key_exists($itemSpecificType, Constants::$translation['item_types']['Weapon']))
-                {
-                    return new Weapon($itemRaw);
-                }
-
-                if (array_key_exists($itemSpecificType, Constants::$translation['item_types']['Armor']))
+                if (array_key_exists($itemSpecificType, $itemTypesArray['Armor']))
                 {
                     return new Armor($itemRaw);
                 }
-                if (array_key_exists($itemSpecificType, Constants::$translation['item_types']['Trinket']))
+
+                if (array_key_exists($itemSpecificType, $itemTypesArray['Consumable']))
+                {
+                    return new Consumable($itemRaw);
+                }
+
+                if (array_key_exists($itemSpecificType, $itemTypesArray['Trinket']))
                 {
                     return new Trinket($itemRaw);
                 }
+
+                if (array_key_exists($itemSpecificType, $itemTypesArray['UpgradeComponent']))
+                {
+                    return new UpgradeComponent($itemRaw);
+                }
+
+                if (array_key_exists($itemSpecificType, $itemTypesArray['Weapon']))
+                {
+                    return new Weapon($itemRaw);
+                }
             }
             $itemType = $itemRaw['type'];
-            if (array_key_exists($itemType, Constants::$translation['item_types']['Bag'])) {
+            if (array_key_exists($itemType, $itemTypesArray['Bag'])) {
                 return new Bag($itemRaw);
             }
 
-            if (array_key_exists($itemType, Constants::$translation['item_types']['Container'])) {
+            if (array_key_exists($itemType, $itemTypesArray['Back']))
+            {
+                return new Back($itemRaw);
+            }
+
+            if (array_key_exists($itemType, $itemTypesArray['Container'])) {
                 return new Container($itemRaw);
+            }
+
+            if (array_key_exists($itemType, $itemTypesArray['CraftingMaterial']))
+            {
+                return new CraftingMaterial($itemRaw);
+            }
+
+            if (array_key_exists($itemType, $itemTypesArray['Trophy']))
+            {
+                return new Trophy($itemRaw);
             }
         }
     }
