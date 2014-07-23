7220ec57c994cb5eee659bdaf62fba6823b8554b
SwHawk <darkdeathangel38@gmail.com>
2014-07-23 17:06:56 +0200

    Suppression de la partie buff, modification de l'héritage de la classe

--- src/SWHawkBot/Entities/BonusItem.php
+++ src/SWHawkBot/Entities/BonusItem.php
@@ -14,7 +14,7 @@ use SWHawkBot\Constants;
  *
  *         @ORM\MappedSuperClass
  */
-class BonusItem extends Item
+class BonusItem extends BuffItem
 {
 
     /**
@@ -131,23 +131,7 @@ class BonusItem extends Item
      */
     protected $vitaliteModifier = null;
 
-    /**
-     * Identifiant du skill de buff de l'objet
-     *
-     * @ORM\Column(type="integer", nullable=true)
-     *
-     * @var integer|nullc
-     */
-    protected $buffSkillId = null;
 
-    /**
-     * Description du skill de buff de l'objet
-     *
-     * @ORM\column(type="text", nullable=true)
-     *
-     * @var string|null
-     */
-    protected $buffSkillDescription = null;
 
     /**
      * Correspondances API/wiki-fr bonus
@@ -362,33 +346,6 @@ class BonusItem extends Item
      * @throws \InvalidArgumentException
      * @return BonusItem
      */
-    public function setBuffSkillId($id)
-    {
-        if (! is_numeric($id)) {
-            throw new \InvalidArgumentException('La fonction attend un id entier. Id donné : ' . var_dump($id));
-        }
-        $this->buffSkillId = $id;
-        return $this;
-    }
-
-    /**
-     *
-     * @param integer $modifier
-     * @throws \InvalidArgumentException
-     * @return BonusItem
-     */
-    public function setBuffSkillDescription($description)
-    {
-        $this->buffSkillDescription = $description;
-        return $this;
-    }
-
-    /**
-     *
-     * @param integer $modifier
-     * @throws \InvalidArgumentException
-     * @return BonusItem
-     */
     public function setUpgradeComponent1Id($id)
     {
         if (! is_numeric($id) && $id != "") {
@@ -503,21 +460,4 @@ class BonusItem extends Item
         return $this->vitaliteModifier;
     }
 
-    /**
-     *
-     * @return integer|null
-     */
-    public function getBuffSkillId()
-    {
-        return $this->buffSkillId;
-    }
-
-    /**
-     *
-     * @return string|null
-     */
-    public function getBuffSkillDescription()
-    {
-        return $this->buffSkillDescription;
-    }
 }
