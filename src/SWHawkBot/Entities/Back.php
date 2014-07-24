d591d600f3b17d2256d51a62da0f9da27643dbf8
SwHawk <darkdeathangel38@gmail.com>
2014-07-24 13:40:46 +0200

    Surcharge du constructeur et du setter de type de la classe parente

--- src/SWHawkBot/Entities/Back.php
+++ src/SWHawkBot/Entities/Back.php
@@ -13,6 +13,15 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Back extends BonusItem
 {
+    public function __construct($back)
+    {
+        parent::__construct($back);
+
+        $this->setType(null);
+
+        return $this;
+    }
+
     public function setType($type)
     {
         $this->type = "Sac à dos";
