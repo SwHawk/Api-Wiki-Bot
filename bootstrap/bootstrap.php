3de928659d69c82d76232f48b780c0e3ec031838
SwHawk <darkdeathangel38@gmail.com>
2014-07-26 14:35:28 +0200

    Ajout du type RarityEnum au bootstrap

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -4,6 +4,7 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Component\Yaml\Parser;
+use Doctrine\DBAL\Types\Type;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,6 +13,8 @@ $isDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration(array(
     __DIR__ . "/src/SWHawkBot/Entities"
 ), $isDevMode, null, null, false);
+
+Type::addType('rarityenum', "SWHawkBot\DBAL\Types\RarityEnumType");
 
 $yamlParser = new Parser();
 
