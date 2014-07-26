c9de080cc8deeda060eaa308532ab9d34031ddac
SwHawk <darkdeathangel38@gmail.com>
2014-07-26 12:27:48 +0200

    Ajout d'un type ENUM pour la rareté

---
+++ src/SWHawkBot/DBAL/Types/RarityEnumType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SWHawkBot\DBAL\Types;
+
+use Doctrine\DBAL\Types\Type;
+
+class RarityEnumType extends Type
+{
+    protected $name = 'rarityenum';
+
+    protected $values = array(
+        "Déchet",
+        "Simple",
+        "Raffiné",
+        "Chef-d'œuvre",
+        "Rare",
+        "Exotique",
+        "Elevé",
+        "Légendaire"
+    );
+
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        $values = array_map(function($val) { return "'".$val."'"; }, $this->values);
+
+        return "ENUM(".implode(", ", $values).") COMMENT '(DC2Type:".$this->name.")'";
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (!in_array($value, $this->values)) {
+            throw new \InvalidArgumentException("Rareté Invalide");
+        }
+        return $value;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+?>
